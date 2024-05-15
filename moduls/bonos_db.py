69578579458f5bed36022441b947ef5797b020b1
Santa <juasanleo3@alu.edu.gva.es>
2024-05-15 19:00:44 +0200

    sin acabar 15052024

---
+++ moduls/bonos_db.py
@@ -0,0 +1,7 @@
+config = { # Configuramos los parámetros para la base de datos
+    'host' : 'db',
+    'port':'3306',
+    'database':'bonos_db',
+    'user' :'root',
+    'password' :'admin'
+    }
