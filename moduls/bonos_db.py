b76883f6865cde1da60c4a3c010264bb5f433d40
JuanAntonioSanta <juasanleo3@alu.edu.gva.es>
2024-05-15 19:09:31 +0200

    Delete moduls directory

--- moduls/bonos_db.py
+++
@@ -1,7 +0,0 @@
-config = { # Configuramos los parámetros para la base de datos
-    'host' : 'db',
-    'port':'3306',
-    'database':'bonos_db',
-    'user' :'root',
-    'password' :'admin'
-    }
